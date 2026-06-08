501fbbfaf38e611b4d5855e3d201f4a20baa5622
Alessio <65780895+alessiob07@users.noreply.github.com>
2026-06-08 11:39:03 +0000

    ombre

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -4,6 +4,6 @@
   let { href, bg = "bg-white", children, ...rest } = $props();
 </script>
 
-<A {href} class="{bg} p-6 rounded-xl shadow-lg m-10 border border-slate-200 block transition hover:scale-[1.02]" {...rest}>
+<A {href} style="box-shadow: 4px 4px 29px 1px #000000;" class="{bg} p-6 rounded-xl shadow-lg m-10 border border-slate-200 block transition hover:scale-[1.02]" {...rest}>
   {@render children()}
 </A>
--- src/routes/fsl/[esperienza]/+page.svelte
+++ src/routes/fsl/[esperienza]/+page.svelte
@@ -4,7 +4,7 @@
 
 <h1>Esperienza presso: {data.meta.titolo}</h1>
 <p>Data: {data.meta.data}</p>
-<p>Nome del tutor: {data.meta.tutor}</p>
+<p>Nome del Tutor: {data.meta.tutor}</p>
 
 <br>
 {@render data.content()}
