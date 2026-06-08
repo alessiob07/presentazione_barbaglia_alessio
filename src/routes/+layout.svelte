a1fd47faf8051ef9caf3572c1a0de8b030b66ab4
Alessio <65780895+alessiob07@users.noreply.github.com>
2026-06-08 11:35:02 +0200

    Update +layout.svelte Indietro

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+ import { page } from '$app/state'
+
+ let isHome = $derivated($page.url.pathname === '/')
 
 	let { children } = $props();
 
@@ -10,9 +13,11 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <div class="min-h-screen bg-slate-50 text-slate-900 font-sans">
+{#if !isHome}
   <nav class="p-6 border-b border-slate-200">
-    <A href="/" class="font-bold text-xl">Home</A>
+    <A href="../" class="font-bold text-xl">Indietro</A>
   </nav>
+{/if}
   <main>
     {@render children()}
   </main>
